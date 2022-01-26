868c009aa5f67f0c1e51c121e2f44eeedad5a5a4
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-01-27 00:16:15 +0900

    add general_user action create show

--- app/controllers/users/general_users_controller.rb
+++ app/controllers/users/general_users_controller.rb
@@ -2,18 +2,33 @@ module Users
   class GeneralUsersController < Users::Base
     def index; end
 
-    def show; end
+    def show
+      @general_user = current_user.general_users.find(params[:id])
+    end
 
     def new
       @general_user = User.new
     end
 
     def create
-      redirect_to users_general_users_url # 仮
+      puts @general_user = User.new(general_user_params)
+      if @general_user.save
+        redirect_to users_general_user_url
+      else
+        render :new
+      end
     end
 
     def edit; end
 
     def update; end
+
+    private
+
+    def general_user_params
+      params.require(:user).permit(
+        :email, :password, :password_confirmation, :name, :age, :gender
+      ).merge(admin_user_id: current_user.id)
+    end
   end
 end
