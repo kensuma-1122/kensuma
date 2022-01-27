99bb9ee53e8916b1acb11160b4424a5758a32768
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-01-28 01:20:03 +0900

    add general_user action edit update destroy

--- app/controllers/users/general_users_controller.rb
+++ app/controllers/users/general_users_controller.rb
@@ -1,19 +1,22 @@
 module Users
   class GeneralUsersController < Users::Base
-    def index; end
+    before_action :set_general_user, only: %i[show edit update destroy]
 
-    def show
-      @general_user = current_user.general_users.find(params[:id])
+    def index
+      @general_users = current_user.general_users
     end
+
+    def show; end
 
     def new
       @general_user = User.new
     end
 
     def create
-      puts @general_user = User.new(general_user_params)
+      @general_user = User.new(general_user_params)
+      @general_user.skip_confirmation! # email認証スキップ
       if @general_user.save
-        redirect_to users_general_user_url
+        redirect_to users_general_user_url(@general_user), flash: { success: '登録しました' }
       else
         render :new
       end
@@ -21,9 +24,25 @@ module Users
 
     def edit; end
 
-    def update; end
+    def update
+      @general_user.skip_reconfirmation! # email認証スキップ
+      if @general_user.update(general_user_params)
+        redirect_to users_general_user_url(@general_user), flash: { success: "#{@general_user.name}を更新しました" }
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @general_user.destroy
+      redirect_to users_general_users_path, flash: { danger: "#{@general_user.name}を削除しました" }
+    end
 
     private
+
+    def set_general_user
+      @general_user = current_user.general_users.find(params[:id])
+    end
 
     def general_user_params
       params.require(:user).permit(
