64ffc013312b06394cac4befb43ced76366b2b57
yusamasaki <masa.aw1208@gmail.com>
2022-02-22 12:32:35 +0900

    orders_controller作成

---
+++ app/controllers/users/orders_controller.rb
@@ -0,0 +1,18 @@
+module Users
+  class OrdersController < Users::Base
+    def index; end
+
+    def show; end
+
+    def new; end
+
+    def edit; end
+
+    def create; end
+
+    def update; end
+
+    def destroy; end
+  end
+end
+
