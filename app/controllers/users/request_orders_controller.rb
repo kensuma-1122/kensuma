7b3d387c0e4ca41724395efc4d6199629497493f
yusamasaki <masa.aw1208@gmail.com>
2022-03-03 07:00:05 +0900

    index・show画面作成

--- app/controllers/users/request_orders_controller.rb
+++ app/controllers/users/request_orders_controller.rb
@@ -1,7 +1,11 @@
 module Users
   class RequestOrdersController < Users::Base
-    def index; end
+    def index
+      @request_orders = current_business.request_orders
+    end
 
-    def show; end
+    def show
+      @request_order = current_business.request_orders.find(params[:id])
+    end
   end
 end
