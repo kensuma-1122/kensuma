f9a0fb722673206f993d5e832a87bbdfabedfb10
yusamasaki <masa.aw1208@gmail.com>
2022-03-03 12:09:03 +0900

    systemspec追加

---
+++ spec/system/request_orders_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'RequestOrders', type: :system do
+  let(:user) { create(:user) }
+  let(:business) { create(:business, user: user) }
+  let(:order) { create(:order, business: business) }
+  let(:request_order) { create(:request_order, business: business, order: order) }
+
+  describe '発注関連' do
+    before(:each) do
+      user.skip_confirmation!
+      user.save!
+      business.save!
+      visit new_user_session_path
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+    end
+
+    context 'ログイン後' do
+      it '発注依頼一覧画面へ遷移できること' do
+        visit users_request_orders_path
+        expect(page).to have_content '発注依頼一覧'
+      end
+
+      it '発注依頼詳細画面に遷移出来る事' do
+        visit users_request_order_path(request_order)
+        expect(page).to have_content '発注依頼詳細'
+        expect(page).to have_content request_order.status_i18n
+      end
+    end
+  end
+end
