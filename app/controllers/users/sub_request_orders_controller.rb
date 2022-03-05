262642c78189da6d94b8ce03fe46f0036534cd96
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-03-05 22:47:28 +0900

    routes設定

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -2,5 +2,18 @@ module Users
   class SubRequestOrdersController < Users::Base
     def index
     end
+
+    def new
+
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
   end
 end
