91f51102b5921d0f3692319602350bff81f5bb92
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-03-06 22:38:58 +0900

    ツリー登録機能作成

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -11,6 +11,12 @@ module Users
     end
 
     def create
+      @sub_request_order = @request_order.order.request_orders.build(sub_request_order_params)
+      if @sub_request_order.save
+        redirect_to users_request_order_url(@request_order)
+      else
+        render :new
+      end
     end
 
     def edit
@@ -23,6 +29,10 @@ module Users
 
     def set_request_order
       @request_order = current_business.request_orders.find_by(uuid: params[:request_order_uuid])
+    end
+
+    def sub_request_order_params
+      params.require(:request_order).permit(:business_id, :parent_id)
     end
   end
 end
