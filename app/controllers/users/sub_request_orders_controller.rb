688f70c7a9ce922e489b7f4d235e9e49e016d150
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-03-06 22:06:03 +0900

    new action作成

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -1,10 +1,13 @@
 module Users
   class SubRequestOrdersController < Users::Base
+    before_action :set_request_order, only: %i[new create]
+
     def index
     end
 
     def new
-
+      @businesses = Business.where.not(id: current_business.id)
+      @sub_request_order = @request_order.order.request_orders.build
     end
 
     def create
@@ -14,6 +17,12 @@ module Users
     end
 
     def update
+    end
+
+    private
+
+    def set_request_order
+      @request_order = current_business.request_orders.find_by(uuid: params[:request_order_uuid])
     end
   end
 end
---
+++ db/fixtures/development/businesses.rb
@@ -0,0 +1,19 @@
+User.first(10).each do |user|
+  Business.seed(:id,
+    {
+      id: user.id,
+      user_id: user.id,
+      uuid: SecureRandom.uuid,
+      name: "サンプル建設#{user.id}",
+      name_kana: "サンプルケンセツイチ#{user.id}",
+      branch_name: "サンプル支店#{user.id}",
+      representative_name: "代表#{user.name}",
+      email: "sample_#{user.id}@email.com",
+      address: "サンプル住所#{user.id}丁目",
+      post_code: "1234567",
+      phone_number: "0312345678",
+      carrier_up_id: "test",
+      business_type: 0
+    }
+  )
+end
