14c9dfe09187031d0dd3ea57ef36d7a9c4796217
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-03-08 22:34:43 +0900

    RequestOrder一括登録機能追加

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -6,17 +6,24 @@ module Users
 
     def new
       @businesses = Business.where.not(id: current_business)
-      @sub_request_order = @request_order.order.request_orders.build
+      @sub_request_order = @request_order.order.request_orders.new
     end
 
     def create
-      @sub_request_order = @request_order.order.request_orders.build(sub_request_order_params)
-      if @sub_request_order.save
-        flash[:success] = '発注依頼を作成しました'
-        redirect_to users_request_order_url(@request_order)
-      else
-        render :new
+      select_businesses = params[:request_order][:business_id]
+
+      select_businesses.each do |select_business|
+        RequestOrder.create!(business_id: select_business, order: @request_order.order, parent_id: @request_order.id)
       end
+      flash[:success] = "#{select_businesses.count}件の発注依頼を作成しました"
+      redirect_to users_request_order_url(@request_order)
+      # @sub_request_order = @request_order.order.request_orders.build(sub_request_order_params)
+      # if @sub_request_order.save
+      #   flash[:success] = '発注依頼を作成しました'
+      #   redirect_to users_request_order_url(@request_order)
+      # else
+      #   render :new
+      # end
     end
 
     def edit; end
@@ -29,8 +36,8 @@ module Users
       @request_order = current_business.request_orders.find_by(uuid: params[:request_order_uuid])
     end
 
-    def sub_request_order_params
-      params.require(:request_order).permit(:business_id).merge(parent_id: @request_order.id)
-    end
+    # def sub_request_order_params
+    #   params.require(:request_order).permit(:business_id).merge(parent_id: @request_order.id)
+    # end
   end
 end
