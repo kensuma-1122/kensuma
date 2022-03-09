57f49763f51bf5478329faedb000b189d028defc
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-09 16:55:41 +0900

    RequestOrder一括登録Transaction処理追加

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -6,24 +6,22 @@ module Users
 
     def new
       @businesses = Business.where.not(id: current_business)
-      @sub_request_order = @request_order.order.request_orders.new
+      @sub_request_order = @request_order.order.request_orders.build
     end
 
     def create
       select_businesses = params[:request_order][:business_id]
 
-      select_businesses.each do |select_business|
-        RequestOrder.create!(business_id: select_business, order: @request_order.order, parent_id: @request_order.id)
+      ActiveRecord::Base.transaction do
+        select_businesses.each do |select_business|
+          sub_request_order = @request_order.order.request_orders.create!(business_id: select_business, parent_id: @request_order.id)
+        end
+        flash[:success] = "#{select_businesses.count}件の発注依頼を作成しました。"
+        redirect_to users_request_order_url(@request_order)
       end
-      flash[:success] = "#{select_businesses.count}件の発注依頼を作成しました"
-      redirect_to users_request_order_url(@request_order)
-      # @sub_request_order = @request_order.order.request_orders.build(sub_request_order_params)
-      # if @sub_request_order.save
-      #   flash[:success] = '発注依頼を作成しました'
-      #   redirect_to users_request_order_url(@request_order)
-      # else
-      #   render :new
-      # end
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = '発注依頼の作成に失敗しました、再度登録してください。'
+      redirect_to new_users_request_order_sub_request_order_path
     end
 
     def edit; end
