061da8a5cf891d07707bf7385741db0e97f8c8a1
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-10 17:28:08 +0900

    view追加

--- app/controllers/users/documents_controller.rb
+++ app/controllers/users/documents_controller.rb
@@ -1,9 +1,14 @@
 module Users
   class DocumentsController < Users::Base
+    # layout 'document'
+
     def show
+      @document = Document.new(created_on: Date.current, submitted_on: Date.current)
     end
 
-    def edit; end
+    def edit
+      @document = Document.new(created_on: Date.current, submitted_on: Date.current)
+    end
 
     def update; end
   end
