e71dbe5a563112b7a56d2eb3ebbac33cbf19d6b2
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-11 13:26:48 +0900

    layout追加,uuidカラム追加

--- app/controllers/users/documents_controller.rb
+++ app/controllers/users/documents_controller.rb
@@ -1,6 +1,10 @@
 module Users
   class DocumentsController < Users::Base
-    # layout 'document'
+    layout 'document'
+
+    def index
+      
+    end
 
     def show
       @document = Document.new(created_on: Date.current, submitted_on: Date.current)
---
+++ db/migrate/20220311041549_add_uuid_to_document.rb
@@ -0,0 +1,5 @@
+class AddUuidToDocument < ActiveRecord::Migration[6.1]
+  def change
+    add_column :documents, :uuid, :string, null: false
+  end
+end
