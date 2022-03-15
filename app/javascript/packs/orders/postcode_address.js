d35ea0f2c3d4640b635709ecc4a2d4367f154a97
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-15 18:02:24 +0900

    modified

--- app/javascript/packs/orders/postcode_address.js
+++ app/javascript/packs/orders/postcode_address.js
@@ -1,8 +1,9 @@
 $(document).on("turbolinks:load", function() {
   $('#order_order_post_code').jpostal({   // 取得する郵便番号を#idで指定
-    postcode : ['#order_order_post_code'], // 取得する郵便番号を#idで指定
+    postcode: ['#order_order_post_code'], // 取得する郵便番号を#idで指定
     // %3 => 都道府県、 %4 => 市区町村 %5 => 町域 %6 => 番地 %7 => 名称
     // それぞれを表示するコントロールをidで指定
-    address : { "#order_order_address"  : "%3%4%5%6%7" }
+    
+    address: { "#order_order_address" : "%3%4%5%6%7" }
   });
 });
