ad8d5abaebd53a5eaca381bb1fa32fd20d0e969a
ikd-gifu <oki19ki@gmail.com>
2022-05-13 12:08:02 +0900

    Add-uuid-column

--- db/fixtures/development/17_news.rb
+++ db/fixtures/development/17_news.rb
@@ -1,37 +1,42 @@
 News.seed(
   {
-    id: SecureRandom.uuid,
+    id: 1,
     title: "お知らせ-1",
     content: Faker::Lorem.sentence(word_count: 20),
     delivered_at: DateTime.now.yesterday,
-    status: 'published'
+    status: 'published',
+    uuid: SecureRandom.uuid
   },
   {
-    id: SecureRandom.uuid,
+    id: 2,
     title: "お知らせ-2",
     content: Faker::Lorem.sentence(word_count: 20),
     delivered_at: DateTime.now.yesterday,
-    status: 'published'
+    status: 'published',
+    uuid: SecureRandom.uuid
   },
   {
-    id: SecureRandom.uuid,
+    id: 3,
     title: "お知らせ-3",
     content: Faker::Lorem.sentence(word_count: 20),
     delivered_at: DateTime.now.yesterday,
-    status: 'published'
+    status: 'published',
+    uuid: SecureRandom.uuid
   },
   {
-    id: SecureRandom.uuid,
+    id: 4,
     title: "お知らせ-4",
     content: Faker::Lorem.sentence(word_count: 20),
     delivered_at: DateTime.now.yesterday,
-    status: 'published'
+    status: 'published',
+    uuid: SecureRandom.uuid
   },
   {
-    id: SecureRandom.uuid,
+    id: 5,
     title: "お知らせ-5",
     content: Faker::Lorem.sentence(word_count: 20),
     delivered_at: DateTime.now.yesterday,
-    status: 'published'
+    status: 'published',
+    uuid: SecureRandom.uuid
   }
 )
---
+++ db/migrate/20220512132159_add_uuid_to_news.rb
@@ -0,0 +1,5 @@
+class AddUuidToNews < ActiveRecord::Migration[6.1]
+  def change
+    add_column :news, :uuid, :string, null: false
+  end
+end
