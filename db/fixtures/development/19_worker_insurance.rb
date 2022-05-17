608ed1451a35e19e0f2b4e426ffee0a834744c45
ikd-gifu <oki19ki@gmail.com>
2022-05-17 20:07:20 +0900

    Fix-worker-insurance

--- db/fixtures/development/19_worker_insurance.rb
+++ db/fixtures/development/19_worker_insurance.rb
@@ -3,13 +3,13 @@ Worker.all.each do |worker|
       {
         id: worker.id,
         worker_id:         worker.id,
-        health_insurance_type:         :health_insurance_association,
-        health_insurance_name:         'サンプル健康保険',
-        pension_insurance_type:        :welfare,
-        employment_insurance_type:     :insured,
-        employment_insurance_number:   '0000',
-        severance_pay_mutual_aid_type: :kentaikyo,
-        severance_pay_mutual_aid_name: 'テスト共済制度'
+        health_insurance_type:         :national_health_insurance,
+        health_insurance_name:         '国民健康保険',
+        pension_insurance_type:        :national,
+        employment_insurance_type:     :day,
+        employment_insurance_number:   '1111',
+        severance_pay_mutual_aid_type: :tyutaikyo,
+        severance_pay_mutual_aid_name: 'サンプル共済制度'
       }
     )
   end
