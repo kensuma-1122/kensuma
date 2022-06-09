179c9eef6f835079e90dbf6a63ea400a342f2b2a
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-06-09 17:32:59 +0900

    add order migration

---
+++ db/migrate/20220609061148_add_sites_to_orders.rb
@@ -0,0 +1,52 @@
+class AddSitesToOrders < ActiveRecord::Migration[6.1]
+  def change
+
+    ## 現場
+    add_column :orders, :site_career_up_id, :string              # 現場ID(キャリアアップ)
+    add_column :orders, :site_address,      :string, null: false # 施工場所(住所)
+
+    ## 発注者
+    add_column :orders, :order_supervisor_name,    :string, null: false # 監督員(氏名)
+    add_column :orders, :order_supervisor_company, :string, null: false # 監督員(所属会社)
+    add_column :orders, :order_supervisor_apply,   :string, null: false # 監督員(権限及び意見の申出方法)
+
+    ## 元請会社
+    add_column :orders, :construction_name,                          :string,  null: false  # 工事名
+    add_column :orders, :construction_details,                       :string,  null: false  # 工事内容
+    add_column :orders, :start_date,                                 :date,    null: false  # 工期(自)
+    add_column :orders, :end_date,                                   :date,    null: false  # 工期(至)
+    add_column :orders, :contract_date,                              :date,    null: false  # 契約日
+    add_column :orders, :submission_destination,                     :string,  null: false  # 提出先及び担当者(部署･氏名)
+    add_column :orders, :general_safety_manager_name,                :string,  null: false  # 統括安全衛生責任者(氏名)
+    add_column :orders, :vice_president_name,                        :string,  null: false  # 副会長(氏名)
+    add_column :orders, :vice_president_company_name,                :string,  null: false  # 副会長(会社名)
+    add_column :orders, :secretary_name,                             :string,  null: false  # 書記(氏名)
+    add_column :orders, :health_and_safety_manager_name,             :string,  null: false  # 元方安全衛生管理者(氏名)
+    add_column :orders, :general_safety_agent_name,                  :string,  null: false  # 統括安全衛生責任者代行者(氏名)
+    add_column :orders, :supervisor_name,                            :string,  null: false  # 監督員(氏名)
+    add_column :orders, :supervisor_apply,                           :string,  null: false  # 監督員(権限及び意見の申出方法)
+    add_column :orders, :site_agent_name,                            :string,  null: false  # 現場代理人(氏名)
+    add_column :orders, :site_agent_apply,                           :string,  null: false  # 現場代理人(権限及び意見の申出方法)
+    add_column :orders, :supervising_engineer_name,                  :string,  null: false  # 監督技術者･主任技術者(氏名)
+    add_column :orders, :supervising_engineer_check,                 :boolean, null: false  # 監督技術者・主任技術者(専任or非専任)〇を付ける
+    add_column :orders, :supervising_engineer_assistant_name,        :string                # 監督技術者補佐(氏名)
+    add_column :orders, :professional_engineer_name,                 :string                # 専門技術者(氏名)
+    add_column :orders, :professional_engineer_construction_details, :string                # 専門技術者(担当工事内容)
+    add_column :orders, :safety_officer_name,                        :string,  null: false  # 安全衛生担当役員(氏名)
+    add_column :orders, :safety_officer_position_name,               :string,  null: false  # 安全衛生担当役員(役職名)
+    add_column :orders, :general_safety_manager_name,                :string                # 総括安全衛生管理(氏名)
+    add_column :orders, :general_safety_manager_position_name,       :string                # 総括安全衛生管理(役職名)
+    add_column :orders, :safety_manager_name,                        :string                # 安全管理者(氏名)
+    add_column :orders, :safety_manager_position_name,               :string                # 安全管理者(役職名)
+    add_column :orders, :health_manager_name,                        :string                # 衛生管理者(氏名)
+    add_column :orders, :health_manager_position_name,               :string                # 衛生管理者(役職名)
+    add_column :orders, :health_and_safety_promoter_name,            :string                # 安全衛生推進者(氏名)
+    add_column :orders, :health_and_safety_promoter_position_name,   :string                # 安全衛生推進者(役職)
+    add_column :orders, :confirm_name,                               :string, null: false   # 確認欄(氏名)
+    add_column :orders, :accept_confirm_date,                        :date,   null: false   # 受付確認(年月日)
+    add_column :orders, :subcontractor_name,                         :string, null: false   # 下請会社名
+
+    ## 共有
+    add_column :orders, :admission_worker_id, :json, null: false # 入場する作業員(複数)作業員ID
+  end
+end
