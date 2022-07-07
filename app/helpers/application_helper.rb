0ce41f20b5425f83e4a16cdaa92ca31880ad7be1
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-07-07 17:43:27 +0900

    helper追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,22 @@ module ApplicationHelper
   def page_body_id(params)
     "#{params[:controller].gsub(/\//, '-')}-#{params[:action]}"
   end
+
+  # 下請け階層表示
+  def sc_hierarchy(request_order)
+    case request_order.depth # .depthメソッドで階層の世代を取得できる。
+    when 0
+      '元請け'
+    when 1
+      '一次下請け'
+    when 2
+      '二次下請け'
+    when 3
+      '三次下請け'
+    when 4
+      '四次下請け'
+    when 5
+      '五次下請け'
+    end
+  end
 end
