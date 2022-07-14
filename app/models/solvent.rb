eb53508fd47b3c73a44f2ba67c557b593e58138a
ikd-gifu <oki19ki@gmail.com>
2022-07-14 17:12:39 +0900

    Add-model-validation

--- app/models/solvent.rb
+++ app/models/solvent.rb
@@ -1,3 +1,9 @@
 class Solvent < ApplicationRecord
   belongs_to :business
+
+  validates :uuid, presence: true
+  validates :name, presence: true
+  validates :maker, presence: true
+  validates :classification, presence: true
+  validates :ingredients, presence: true  
 end
